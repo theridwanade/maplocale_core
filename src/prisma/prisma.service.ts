9fb4b21374649452e839463bf41f3e348ed9f1d7
theridwanade <theridwanade@gmail.com>
2026-03-19 15:47:01 +0100

    Update Prisma adapter to use PrismaPg

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaClient } from "./generated/client";
-import { PrismaPostgresAdapter } from "@prisma/adapter-ppg";
 import { TypedConfigService } from "src/config/typed-config.service";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor(private readonly configService: TypedConfigService) {
-    const adapter = new PrismaPostgresAdapter({
+    const adapter = new PrismaPg({
       connectionString: configService.get("DATABASE_URL"),
     });
     super({ adapter });
