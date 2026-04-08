4b7a77dc89b1b2b5f2692822bc01623f7591dd4c
theridwanade <theridwanade@gmail.com>
2026-04-09 00:22:35 +0100

    Added ready route for readiness health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from "@nestjs/common";
 import { ModulesModule } from "./modules/modules.module";
 import { ConfigModule } from "./config/config.module";
 import { PrismaModule } from "./prisma/prisma.module";
+import { AppController } from "./app.controller";
 
 @Module({
   imports: [ConfigModule, ModulesModule, PrismaModule],
+  controllers: [AppController],
 })
 export class AppModule {}
